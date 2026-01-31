a2a12e07fac4cf112226965526aad4017705b33d
antonymaks <antonymaks@yandex.ru>
2026-01-31 17:29:31 +0300

    Autotest err fix 2

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -3,7 +3,9 @@
 package daysteps
 
 import (
+	"errors"
 	"fmt"
+	"log"
 	"strconv"
 	"strings"
 	"time"
@@ -18,28 +20,44 @@ const (
 	mInKm = 1000
 )
 
+var err error
+
 // Парсит строку активности,
 // возвращает кол-во шагов, время прогулки и возможную ошибку.
 func parsePackage(data string) (int, time.Duration, error) {
 	dataSlice := strings.Split(data, ",")
-	if len(dataSlice) != 2 {
-		return 0, 0, fmt.Errorf("incorrect input data format")
+	if len(dataSlice) == 2 {
+		stepsCount, err := strconv.Atoi(dataSlice[0])
+		if err != nil {
+			err = errors.New("fail to conver string to integer")
+			log.Println(err)
+			return 0, 0, err
+
+		}
+		if stepsCount <= 0 {
+			err = errors.New("amount of steps must be greater, than zero")
+			log.Println(err)
+			return 0, 0, err
+		}
+
+		walkDuration, err := time.ParseDuration(dataSlice[1])
+		if err != nil {
+			err = errors.New("fail to parse time")
+			log.Println(err)
+			return 0, 0, err
+		}
+		if walkDuration <= 0 {
+			err = errors.New("duration of training must be greater, than zero")
+			log.Println(err)
+			return 0, 0, err
+		}
+
+		return stepsCount, walkDuration, nil
 	}
 
-	stepsCount, err := strconv.Atoi(dataSlice[0])
-	if err != nil {
-		return 0, 0, err
-	}
-	if stepsCount <= 0 {
-		return 0, 0, fmt.Errorf("amount of steps must be greater, than zero")
-	}
-
-	walkDuration, err := time.ParseDuration(dataSlice[1])
-	if err != nil {
-		return 0, 0, err
-	}
-
-	return stepsCount, walkDuration, nil
+	err = errors.New("incorrect data format")
+	log.Println(err)
+	return 0, 0, err
 }
 
 // Парсит строку при помощи parsePackage,
@@ -48,7 +66,8 @@ func parsePackage(data string) (int, time.Duration, error) {
 func DayActionInfo(data string, weight, height float64) string {
 	steps, duration, err := parsePackage(data)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
+		fmt.Println("error in DayActionInfo: ", err)
 		return ``
 	}
 
@@ -56,10 +75,12 @@ func DayActionInfo(data string, weight, height float64) string {
 	distanceKm := distanceMeters / mInKm
 	calories, err := spentcalories.WalkingSpentCalories(steps, weight, height, duration)
 	if err != nil {
-		outputString := fmt.Sprintf("Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.",
-			steps, distanceKm, calories)
-		return outputString
-
+		log.Println(err)
+		fmt.Println("error occured: ", err)
 	}
-	return ``
+
+	outputString := fmt.Sprintf("Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.\n",
+		steps, distanceKm, calories)
+	return outputString
+
 }
